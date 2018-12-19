f5200d193caba11a0b7b30bcd1dc3d7b6a7586c9
Richard L Zarth III <rlziii@icloud.com>
2018-12-18 21:49:09 -0500

    Initial Commit

---
+++ ChessMoves/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ChessMoves
+//
+//  Created by Richard L Zarth III on 2018-12-18.
+//  Copyright © 2018 RLZIII. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
